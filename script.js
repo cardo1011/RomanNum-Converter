894c3fc02dd3e5d69a4eee9a9c66e0a646a1ab08
cardo1011 <117056167+cardo1011@users.noreply.github.com>
2024-05-18 19:13:50 -0700

    finito

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const button = document.getElementById("convert-btn");
 const resultBox = document.getElementById("output");
 
+resultBox.style.visibility = "hidden";
+
 const map = new Map();
 
 for (i = 1; i < 4; i++) {
@@ -20,9 +22,42 @@ map.set(100, "C");
 map.set(400, "CD");
 map.set(500, "D");
 map.set(900, "CM");
-map.set(1000, "1000");
-console.log(map);
+map.set(1000, "M");
+// console.log(map);
+
+const romanValues = [...map.keys()].sort((a, b) => b - a);
+
+console.log(romanValues);
 
 function romanConverter() {
-  let input = document.getElementById("number");
+  let input = document.getElementById("number").value;
+  let result = "";
+
+  if (!input) {
+    resultBox.style.visibility = "visible";
+    resultBox.style.fontSize = "";
+    return "Please enter a valid number";
+  } else if (input < 1) {
+    resultBox.style.visibility = "visible";
+    resultBox.style.fontSize = "";
+    return "Please enter a number greater than or equal to 1";
+  } else if (input > 3999) {
+    resultBox.style.visibility = "visible";
+    resultBox.style.fontSize = "";
+    return "Please enter a number less than or equal to 3999";
+  } else {
+    romanValues.forEach((value) => {
+      while (input >= value) {
+        result += map.get(value);
+        input -= value;
+      }
+    });
+    resultBox.style.visibility = "visible";
+    resultBox.style.fontSize = "3em";
+    return result;
+  }
 }
+
+button.addEventListener("click", function () {
+  resultBox.textContent = romanConverter();
+});
